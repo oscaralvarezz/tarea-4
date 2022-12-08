7ddb26a350826e5f0f7e3e3e43f4e1f584984681
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2022-12-08 17:29:32 +0100

    actualizar ej4

--- ejercicios/ej4.py
+++ ejercicios/ej4.py
@@ -15,7 +15,7 @@ def biseccion(a,b,tol):
                 a=c
         return c
         
-print (biseccion(10,10))
+print (biseccion(8,8))
 
 def f(x):
     return math.exp(-x) -x
@@ -27,8 +27,7 @@ def secante(x0,x1,tol):
         x1=x2
     return x2
 
-print (secante(10,10))
-
+print (secante(8,8))
 
 def f(x):
     return math.exp(-x) -x
@@ -40,8 +39,7 @@ def newton (x0,tol):
     while abs(f(x0))>tol:
         x0=x0-f(x0)/df(x0)
     return x0
-
-print (newton(10,10))
+print (newton(8,8))
 
 def f(x):
     return x**3 + x + 16
@@ -51,7 +49,7 @@ def df(x):
 
 def biseccion(a,b,tol):
     if f(a)*f(b)>0:
-        print ("No existe una raíz en este intervalo")
+        print ("No existe raíz en este intervalo")
     else:
         while abs(b-a)>tol:
             c=(a+b)/2.0
@@ -73,6 +71,6 @@ def newton_raphson (x0,tol):
         x0=x0-f(x0)/df(x0)
     return x0
 
-print ("Bisección: ",biseccion(10,10))
-print ("Secante: ",secante(10,10))
-print ("Newton-Raphson: ",newton_raphson(10,10))
+print ("Bisección: ",biseccion(8,8))
+print ("Secante: ",secante(8,8))
+print ("NewtonRaphson: ",newton_raphson(8,8))
